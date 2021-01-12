f55804ad0de0f961c7e97ac4bb54784a752379d3
Invizabel <46697708+Invizabel@users.noreply.github.com>
2021-01-11 21:36:22 -0600

    Add files via upload

--- Python Engines/Web Engine/Web Engine.py
+++ Python Engines/Web Engine/Web Engine.py
@@ -27,29 +27,47 @@ def no_log():
 	print("")
 	print("Cookies: " + str(final.cookies))
 
+	final.close()
+
 	print("")
 	print("Encoding: " + str(final.encoding))
+
+	final.close()
 
 	print("")
 	print("Headers: " + str(final.headers))
 
+	final.close()
+
 	print("")
 	print("Is permanent redirect: " + str(final.is_permanent_redirect))
+
+	final.close()
 
 	print("")
 	print("Is redirect: " + str(final.is_redirect))
 
+	final.close()
+
 	print("")
 	print("Ok: " + str(final.ok))
+
+	final.close()
 
 	print("")
 	print("Reason: " + str(final.reason))
 
+	final.close()
+
 	print("")
 	print("Status code: " + str(final.status_code))
 
+	final.close()
+
 	print("")
 	print("Text: " + str(final.text))
+
+	final.close()
 
 	print("")
 	print("URL: " + str(final.url))
@@ -73,37 +91,55 @@ def log():
 	print("Cookies: " + str(final.cookies))
 	file.write("Cookies: " + str(final.cookies))
 
+	final.close()
+
 	print("")
 	print("Encoding: " + str(final.encoding))
 	file.write("\n\nEncoding: " + str(final.encoding))
+
+	final.close()
 
 	print("")
 	print("Headers: " + str(final.headers))
 	file.write("\n\nHeaders: " + str(final.headers))
 
+	final.close()
+
 	print("")
 	print("Is permanent redirect: " + str(final.is_permanent_redirect))
 	file.write("\n\nIs permanent redirect: " + str(final.is_permanent_redirect))
+
+	final.close()
 
 	print("")
 	print("Is redirect: " + str(final.is_redirect))
 	file.write("\n\nIs redirect: " + str(final.is_redirect))
 
+	final.close()
+
 	print("")
 	print("Ok: " + str(final.ok))
 	file.write("\n\nOk: " + str(final.ok))
+
+	final.close()
 
 	print("")
 	print("Reason: " + str(final.reason))
 	file.write("\n\nReason: " + str(final.reason))
 
+	final.close()
+
 	print("")
 	print("Status code: " + str(final.status_code))
 	file.write("\n\nStatus code: " + str(final.status_code))
 
+	final.close()
+
 	print("")
 	print("Text: " + str(final.text))
 	file.write("\n\nText: " + str(final.text))
+
+	final.close()
 
 	print("")
 	print("URL: " + str(final.url))

b4155fd029c42c7b7dc09a6f7d69831117c004b4
Invizabel <46697708+Invizabel@users.noreply.github.com>
2021-02-07 18:30:19 -0600

    Add files via upload

--- Python Engines/Web Engine/Web Engine.py
+++ Python Engines/Web Engine/Web Engine.py
@@ -1,9 +1,18 @@
 #Documentation:
 #https://www.geeksforgeeks.org/downloading-files-web-using-python/
 #https://www.w3schools.com/PYTHON/ref_requests_get.asp
+#https://www.geeksforgeeks.org/python-check-url-string/
+#https://www.w3schools.com/python/gloss_python_check_string.asp
 
 import os
+import re
 import requests
+import time
+
+def find_url(string):
+	regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+	url = re.findall(regex,string)       
+	return [x[0] for x in url] 
 
 def web_engine():
 	os.system("clear")
@@ -20,7 +29,7 @@ def web_engine():
 	if user_input == "3":
 		os.system("clear")
 
-		print("1 = image | 2 = pdf")
+		print("1 = image | 2 = pdf | 3 = all images")
 		user_file = input()
 
 		if user_file == "1":
@@ -28,6 +37,9 @@ def web_engine():
 
 		if user_file == "2":
 			pdf()
+
+		if user_file == "3":
+			all_images()
 
 def no_log():
 	secure = ""
@@ -38,6 +50,8 @@ def no_log():
 	user_input = input()
 	print("\nSecure? y/n")
 	secure_input = input()
+
+	start = time.time()
 
 	if secure_input == "y":
 		secure = "https://"
@@ -97,6 +111,9 @@ def no_log():
 	print("")
 	print("URL: " + str(final.url))
 
+	end = time.time()
+	print("\nTime: " + str(end - start) + " seconds.")
+
 	final.close()
 
 def log():
@@ -110,6 +127,8 @@ def log():
 	user_input = input()
 	print("\nSecure? y/n")
 	secure_input = input()
+
+	start = time.time()
 
 	if secure_input == "y":
 		secure = "https://"
@@ -179,6 +198,10 @@ def log():
 	print("URL: " + str(final.url)) 
 	file.write("\n\nURL: " + str(final.url) + "\n\n")
 
+	end = time.time()
+	print("\nTime: " + str(end - start) + " seconds.")
+	file.write("\nTime: " + str(end - start) + " seconds.")
+
 	final.close()
 	file.close()
 
@@ -193,6 +216,11 @@ def image():
 	name = input()
 	print("\nSecure? y/n")
 	secure_input = input()
+
+	os.system("clear")
+	print("Downloading!")
+
+	start = time.time()
 
 	if secure_input == "y":
 		secure = "https://"
@@ -209,6 +237,9 @@ def image():
 	with open(name, "wb") as file_writer:
 		file_writer.write(data.content)
 
+	end = time.time()
+	print("\nTime: " + str(end - start) + " seconds.")
+
 def pdf():
 	secure = ""
 
@@ -220,6 +251,11 @@ def pdf():
 	name = input()
 	print("\nSecure? y/n")
 	secure_input = input()
+
+	os.system("clear")
+	print("Downloading!")
+
+	start = time.time()
 
 	if secure_input == "y":
 		secure = "https://"
@@ -237,5 +273,99 @@ def pdf():
 		for chunk in data.iter_content(chunk_size=1024):
 			if chunk:
              			pdf.write(chunk)
+
+	end = time.time()
+	print("\nTime: " + str(end - start) + " seconds.")
+
+def all_images():
+	count = 0
+	secure = ""
+
+	os.system("clear")
+
+	print("Enter website:")
+	user_input = input()
+	print("\nSecure? y/n")
+	secure_input = input()
+
+	os.system("clear")
+	print("Downloading!")
+
+	start = time.time()
+
+	if secure_input == "y":
+		secure = "https://"
+
+	if secure_input == "n":
+		secure = "http://"
+
+	output = secure + user_input
+
+	url = requests.get(output, verify = True)
+	out = str(url.text)
+	web_list = find_url(out)
+
+	for i in web_list:
+		jpeg = "jpeg" in i
+		jpg = "jpg" in i
+		png = "png" in i
+		y = "http" in i
+
+		if jpeg == True and y == True:
+			count += 1
+
+			picture = str(i)
+			data = requests.get(picture, verify = True)
+
+			with open("image " + str(count)  + ".jpeg", "wb") as file_writer:
+				file_writer.write(data.content)
+
+		if jpeg == True and y == False:
+			count += 1
+
+			picture = secure + str(i)
+			data = requests.get(picture, verify = True)
+
+			with open("image " + str(count)  + ".jpeg", "wb") as file_writer:
+				file_writer.write(data.content)
+
+		if jpg == True and y == True:
+			count += 1
+
+			picture = str(i)
+			data = requests.get(picture, verify = True)
+
+			with open("image " + str(count)  + ".jpg", "wb") as file_writer:
+				file_writer.write(data.content)
+
+		if jpg == True and y == False:
+			count += 1
+
+			picture = secure + str(i)
+			data = requests.get(picture, verify = True)
+
+			with open("image " + str(count)  + ".jpg", "wb") as file_writer:
+				file_writer.write(data.content)
+
+		if png == True and y == True:
+			count += 1
+
+			picture = str(i)
+			data = requests.get(picture, verify = True)
+
+			with open("image " + str(count)  + ".png", "wb") as file_writer:
+				file_writer.write(data.content)
+
+		if png == True and y == False:
+			count += 1
+
+			picture = secure + str(i)
+			data = requests.get(picture, verify = True)
+
+			with open("image " + str(count)  + ".png", "wb") as file_writer:
+				file_writer.write(data.content)
+
+	end = time.time()
+	print("\nTime: " + str(end - start) + " seconds.")
 
 web_engine()
